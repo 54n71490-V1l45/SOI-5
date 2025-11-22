92ffbe6bbb6274e5e1c379282177daa67f90b034
54n71490-V1l45 <santiago.vilas@rai.usc.es>
2025-11-22 12:40:19 +0100

    Actualizaciónd el ejercicio 2

--- main2.c
+++ main2.c
@@ -8,30 +8,36 @@ Autores:
 #include <stdlib.h>
 #include <unistd.h>
 
-
 void f1(int a){
     int c=3;
-    printf("direccion de memoria de fi: %p\n", f1);
+    printf("direccion de memoria de f1: %p\n", f1);
     printf("dirección de memoria de a en f1: %p\n", &a);
     printf("dirección de memoria de c (local) en f1: %p\n", &c);
     printf("Valor de a en f1: %d\n", a);
     printf("Valor de c en f1: %d\n", c);
+    return;
 }
 
 void f2(int b){
     int d=4;
-    printf("direccion de memoria de fi: %p\n", f2);
+    printf("direccion de memoria de f2: %p\n", f2);
     printf("dirección de memoria de b en f2: %p\n", &b);
     printf("dirección de memoria de d (local) en f2: %p\n", &d);
     printf("Valor de b en f2: %d\n", b);
     printf("Valor de d en f2: %d\n", d);
+    return;
 }
+
 int main() {
     int a=1, b=2;
     printf("PID: %d\n", getpid());
     printf("direccion de memoria de main: %p\n", main);
     f1(a);
     f2(b);
+    printf("dirección de memoria de a en main: %p\n", &a);
+    printf("dirección de memoria de b en main: %p\n", &b);
+
+    printf("Pausa para poder mirar el mapa (pulsa enter para seguir)\n");
     scanf("%*c");
-    return 0;
+    exit(0);
 }
